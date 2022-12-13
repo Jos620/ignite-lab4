1f3bbf789281649912e2d7395dc6ddbf956bef99
Mateus Ito <mateusito10@gmail.com>
2022-12-13 09:50:27 -0300

    feat(prisma): add prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('prisma')
+  getPrisma() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post('prisma')
+  async postPrisma() {
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Hello World',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
