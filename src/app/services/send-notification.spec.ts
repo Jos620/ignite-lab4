4010f188e7a02bb30e069a611c777b69ed198a1b
Mateus Ito <mateusito10@gmail.com>
2022-12-17 11:14:20 -0300

    test: add test for send-notification service

--- src/app/services/send-notification.spec.ts
+++ src/app/services/send-notification.spec.ts
@@ -1,16 +1,20 @@
-import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 
 describe('Send notification', () => {
-  const sendNotification = new SendNotification();
-
   it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+
     const { notification } = await sendNotification.execute({
       category: 'social',
       content: 'Hello world',
       recipientId: 'example-id',
     });
 
-    expect(notification).toBeInstanceOf(Notification);
+    const allNotifications = await notificationsRepository.getAll();
+
+    expect(allNotifications).toHaveLength(1);
+    expect(allNotifications[0]).toEqual(notification);
   });
 });
