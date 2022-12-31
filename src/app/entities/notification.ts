9290d0eb918e965d98a82d1c250217864db3d92c
Mateus Ito <mateusito10@gmail.com>
2022-12-31 12:31:06 -0300

    feat: add canceledAt prop to Notification

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -7,6 +7,7 @@ interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -60,5 +61,13 @@ export class Notification {
 
   public get createdAt(): Date {
     return this.props.createdAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
   }
 }
