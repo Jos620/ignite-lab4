19ba2d9c8f746d35285c42b6c2529a748f13a47c
Mateus Ito <mateusito10@gmail.com>
2022-12-31 12:16:43 -0300

    feat: add Notification view model

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SendNotification } from '@app/services/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -10,8 +11,10 @@ export class NotificationsController {
   async create(@Body() body: CreateNotificationBody) {
     const { notification } = await this.sendNotification.execute(body);
 
+    const response = NotificationViewModel.toHTTP(notification);
+
     return {
-      notification,
+      notification: response,
     };
   }
 }
